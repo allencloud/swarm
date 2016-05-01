01764c8ef0a925ea38da175ab9dab616bfb1001e
Sun Hongliang <allen.sun@daocloud.io>
2016-05-01 23:57:19 +0800

    return detailed error when two volumes have the same name
    Signed-off-by: Sun Hongliang <allen.sun@daocloud.io>

--- cluster/volume.go
+++ cluster/volume.go
@@ -17,9 +17,9 @@ type Volume struct {
 type Volumes []*Volume
 
 // Get returns a volume using its ID or Name
-func (volumes Volumes) Get(name string) (*Volume, error) {
-	// Abort immediately if the name is empty.
-	if len(name) == 0 {
+func (volumes Volumes) Get(IDOrName string) (*Volume, error) {
+	// Abort immediately if the IDOrName is empty.
+	if len(IDOrName) == 0 {
 		return nil, fmt.Errorf("Length of volume name cannot be 0")
 	}
 
@@ -27,7 +27,9 @@ func (volumes Volumes) Get(name string) (*Volume, error) {
 
 	// Match name or engine/name.
 	for _, volume := range volumes {
-		if volume.Name == name || volume.Engine.ID+"/"+volume.Name == name || volume.Engine.Name+"/"+volume.Name == name {
+		if volume.Name == IDOrName ||
+			volume.Engine.ID+"/"+volume.Name == IDOrName ||
+			volume.Engine.Name+"/"+volume.Name == IDOrName {
 			candidates = append(candidates, volume)
 		}
 	}
@@ -36,21 +38,15 @@ func (volumes Volumes) Get(name string) (*Volume, error) {
 	if size := len(candidates); size == 1 {
 		return candidates[0], nil
 	} else if size > 1 {
-		// Match first volume with non-local driver
-		for _, volume := range candidates {
-			if volume.Name == name && volume.Driver != "local" {
-				return volume
-			}
-		}
-		return nil, fmt.Errorf("More than one volume named by (%s) in cluster." +
-			" Please use Engine_ID/Volume_Name to specify one.", name)
+		return nil, fmt.Errorf("More than one volume named by (%s) in cluster."+
+			" Please use Engine_ID/Volume_Name to specify one.", IDOrName)
 	}
 
 	// There is no candidate.
 	// Match /name and return as soon as we find one.
 	for _, volume := range volumes {
-		if volume.Name == "/"+name {
-			return volume
+		if volume.Name == "/"+IDOrName {
+			candidates = append(candidates, volume)
 		}
 	}
 
